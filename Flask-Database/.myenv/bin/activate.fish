1c0f8a0cb550e1e60724ef205d0f10e0f32c9d26
ROHAN-88 <therohanshrestha6655@gmail.com>
2025-03-02 15:13:51 +0545

    Changes have been made

--- Flask-Database/.myenv/bin/activate.fish
+++ Flask-Database/.myenv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/rohan/Documents/Chat-App/Flask-Database/.myenv
+set -gx VIRTUAL_ENV /home/rohan/Documents/CHAT-App-Fullstack-/Flask-Database/.myenv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
